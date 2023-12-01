0a96163cdf9cf5a4c53de7d5da26578b8a4a1d63
John Downey <jdowney@gmail.com>
2023-12-01 11:02:22 -0600

    Clean up day 1

--- advent-2023/src/day1.rs
+++ advent-2023/src/day1.rs
@@ -1,32 +1,27 @@
 use aoc_runner_derive::aoc;
 
-const NUMBER_STRINGS: [&str; 18] = [
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4",
-    "5", "6", "7", "8", "9",
+const NUMBERS: [&str; 9] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const NUMBER_STRINGS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
 
-fn is_digits(number: &str) -> bool {
-    number.chars().all(|c| c.is_ascii_digit())
-}
-
 fn number_string_to_value(number: &str) -> Option<u32> {
-    match number {
-        "one" => Some(1),
-        "two" => Some(2),
-        "three" => Some(3),
-        "four" => Some(4),
-        "five" => Some(5),
-        "six" => Some(6),
-        "seven" => Some(7),
-        "eight" => Some(8),
-        "nine" => Some(9),
-        s if is_digits(s) => s.parse().ok(),
-        _ => None,
+    if let Ok(n) = number.parse::<u32>() {
+        return Some(n);
     }
+
+    if let Some(index) = NUMBER_STRINGS.iter().position(|&item| item == number) {
+        return Some((index + 1) as u32);
+    }
+
+    None
 }
 
-fn find_number(line: &str, reverse: bool) -> Option<u32> {
-    let matches = NUMBER_STRINGS.iter().filter_map(|p| {
+fn find_number<'a, S>(line: &str, search: S, reverse: bool) -> Option<u32>
+where
+    S: Iterator<Item = &'a str>,
+{
+    let matches = search.filter_map(|p| {
         if reverse {
             line.rmatch_indices(p).next()
         } else {
@@ -45,25 +40,28 @@ fn find_number(line: &str, reverse: bool) -> Option<u32> {
 
 #[aoc(day1, part1)]
 fn part1(input: &str) -> Option<u32> {
+    let search = NUMBERS.iter().copied();
     input
         .lines()
         .map(|line| {
-            let bstr = line.as_bytes();
-            let tens = *bstr.iter().find(|c| c.is_ascii_digit())?;
-            let ones = *bstr.iter().rfind(|c| c.is_ascii_digit())?;
-
-            Some(((tens - b'0') * 10 + (ones - b'0')) as u32)
+            let tens = find_number(line, search.clone(), false)?;
+            let ones = find_number(line, search.clone(), true)?;
+            Some(tens * 10 + ones)
         })
         .sum()
 }
 
 #[aoc(day1, part2)]
 fn part2(input: &str) -> Option<u32> {
+    let search = NUMBERS
+        .iter()
+        .copied()
+        .chain(NUMBER_STRINGS.iter().copied());
     input
         .lines()
         .map(|line| {
-            let tens = find_number(line, false)?;
-            let ones = find_number(line, true)?;
+            let tens = find_number(line, search.clone(), false)?;
+            let ones = find_number(line, search.clone(), true)?;
             Some(tens * 10 + ones)
         })
         .sum()
