e26654f00e3ada632dc34ec195a38fcb427798e6
John Downey <jdowney@gmail.com>
2024-12-01 23:32:50 -0600

    Add day 2 of 2024

---
+++ advent-2024/src/day2.rs
@@ -0,0 +1,61 @@
+use std::num::ParseIntError;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
+
+type Report = Vec<i32>;
+
+#[aoc_generator(day2)]
+fn generator(input: &str) -> Result<Vec<Report>, ParseIntError> {
+    input
+        .lines()
+        .map(|line| line.split_whitespace().map(|s| s.parse()).collect())
+        .collect()
+}
+
+fn is_safe(report: &Report) -> bool {
+    fn is_valid_diff(a: i32, b: i32) -> bool {
+        let diff = a.abs_diff(b);
+        (1..=3).contains(&diff)
+    }
+
+    let mut trend = None;
+
+    for (&a, &b) in report.iter().tuple_windows() {
+        if !is_valid_diff(a, b) {
+            return false;
+        }
+
+        match trend {
+            Some(true) if a > b => return false,
+            Some(false) if a < b => return false,
+            None => trend = Some(a <= b),
+            _ => continue,
+        }
+    }
+
+    true
+}
+
+#[aoc(day2, part1)]
+fn part1(input: &[Report]) -> usize {
+    input.iter().filter(|report| is_safe(report)).count()
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &[Report]) -> usize {
+    input
+        .iter()
+        .filter(|&report| {
+            if is_safe(report) {
+                return true;
+            }
+
+            (0..report.len()).any(|i| {
+                let mut reduced = report.clone();
+                reduced.remove(i);
+                is_safe(&reduced)
+            })
+        })
+        .count()
+}
